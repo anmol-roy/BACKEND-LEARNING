d3f056ec42b9acf6748a0471e42dc0c4676ba858
Anmol Kumar <royanmol112@gmail.com>
2025-10-17 11:38:50 +0530

    form handling block working comleted

--- 4/index.js
+++ 4/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
