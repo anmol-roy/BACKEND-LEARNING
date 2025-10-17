dfd03224c1147b9a0264a2daf1a2751463b91648
Anmol Kumar <royanmol112@gmail.com>
2025-10-17 11:23:53 +0530

    routing and middleware

--- 4/index.js
+++ 4/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.get( '/' , function ( req, res){
     res.send('Hello Worl    d!');
-}
-
-);
+});
 app.listen(3000)
